90e2235eda94e3293fdd064cb7a9a1fc7e3c8ef3
dannybas <dannyb@wix.com>
2020-12-08 15:52:10 +0200

    landing page draft1

---
+++ landing/sw.js
@@ -0,0 +1,23 @@
+var cacheName = 'Wix Mobile Community';
+var filesToCache = [
+  './',
+  './index.html',
+];
+
+// start the service and cache content
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// serve cached content when offline
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
